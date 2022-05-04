dafec218dd04b38decd674aa1e106ab09d3e1b7f
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-04 13:10:18 +0200

    break stuff

--- stuff/turtle_ai.js
+++ stuff/turtle_ai.js
@@ -1,4 +1,4 @@
-class AI{
+class ScriptAI {
     constructor(code,world,robot){
         this.code=code.split("\n").map(l=>l.trim());
         this.world = world;
@@ -8,10 +8,10 @@ class AI{
         this.line=0;
     }
     tick(){
+        this.area=getSubgrid();
         if(this.state.timeoutUntil>Date.now()){
             return;
         }
-
             let line=this.code[this.line];
             let tokens=line.split(" ");
             let command=tokens.shift();
@@ -47,6 +47,15 @@ class AI{
                 let num=token.substring(6,token.length-1);
                 tokens[i]=Math.floor(Math.random()*num);
             }
+            //read from tiles
+            if(token.startsWith("$tile(")){
+                let x=token.substring(6,token.indexOf(","));
+                let y=token.substring(token.indexOf(",")+1,token.length-1);
+                //parse x and y
+                x=parseInt(x);
+                y=parseInt(y);
+                tokens[i]=this.area[x][y];
+            }
 
             if(token.startsWith("$")){
                 let varName=token.substring(1);
@@ -57,12 +66,37 @@ class AI{
         }
         return tokens;
     }
-
+    getSubgrid(){
+        //return blocked tiles in a 9x9 grid around the robot
+        let area=[];
+        for(let x=this.robot.x-4;x<this.robot.x+5;x++){
+            let row=[];
+            for(let y=this.robot.y-4;y<this.robot.y+5;y++){
+                if(x<0||y<0||x>=this.world.width||y>=this.world.height){
+                    row.push(true);
+                }
+                else{
+                    row.push(this.world.getTile(x,y).blocked);
+                }
+            }
+            area.push(row);
+        }
+        return area;
+    }
 }
+
+
+
+
+
+
+
 
 let instructions = {
     move: moveFunction,
-    wait: waitFunction
+    wait: waitFunction,
+    set: setVarFunction,
+    add: addFunction
 }
 
 function moveFunction(robot,args){
@@ -75,7 +109,30 @@ function waitFunction(robot,args){
     let timeout = parseInt(args[0]);
     robot.ai.state.timeoutUntil = Date.now()+timeout;
 }
-
+function setVarFunction(robot,args){
+    let varName = args[0];
+    let varValue = args[1];
+    //parse as int
+    if(varValue.startsWith("$")){
+        varValue = robot.ai.vars[varValue.substring(1)];
+    }
+    else{
+        varValue = parseInt(varValue);
+    }
+    robot.ai.vars[varName]=varValue;
+}
+function addFunction(robot,args){
+    let varName = args[0];
+    let varValue = args[1];
+    //parse as int
+    if(varValue.startsWith("$")){
+        varValue = robot.ai.vars[varValue.substring(1)];
+    }
+    else{
+        varValue = parseInt(varValue);
+    }
+    robot.ai.vars[varName]+=varValue;
+}
 
 
 
