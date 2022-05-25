283f53708899ea1216da6c029e8dcdb06d0569bb
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-25 12:29:01 +0200

    about to scrap it

--- games/engine/engine.mjs
+++ games/engine/engine.mjs
@@ -29,11 +29,21 @@ class vector{
         this.x=x;
         this.y=y;
     }
-    get length(){
+    get radius(){
         return Math.sqrt(this.x*this.x+this.y*this.y);
+    }
+    set radius(length){
+        let angle = this.angle;
+        this.x = Math.cos(angle) * length;
+        this.y = Math.sin(angle) * length;
     }
     get angle(){
         return Math.atan2(this.y,this.x);
+    }
+    set angle(angle){
+        let length = this.radius;
+        this.x = Math.cos(angle) * length;
+        this.y = Math.sin(angle) * length;
     }
     distanceTo(pos){
         return Math.sqrt(Math.pow(this.x-pos.x,2)+Math.pow(this.y-pos.y,2));
@@ -44,47 +54,45 @@ class vector{
     relativeTo(pos){
         return new pos(this.x-pos.x,this.y-pos.y);
     }
+    translate(angle,distance){
+        this.x+=Math.cos(angle)*distance;
+        this.y+=Math.sin(angle)*distance;
+    }
     toJSON(){
         return {type:"vector",x:this.x,y:this.y};
+    }
+    static fromJSON(json){
+        return new vector(json.x,json.y);
     }
 }
 
 
 class gameObject{
-    constructor(x,y,w,h){
+    constructor(x,y){
         this.id=Math.floor(Math.random()*1000000);
+        this.groupId=0;
         this.pos=new pos(x,y);
-        this.w=w;
-        this.h=h;
         this.rotation=0;
         this.dead=false;
         this.collides=false;
         this.static=true;
-        this.eventListeners=[];
     }
     update(delta){
-        //override
     }
 }
 class movingObject extends gameObject{
-    constructor(x,y,w,h,vel,){
+    constructor(x,y,w,h,mass,vel){
         super(x,y,w,h);
-        this.speed=speed;
-        this.vel=new pos(0,0);
+        this.vel=new vector(vel.x,vel.y);
+        this.rotationRate=0;
+        this.mass=mass;
     }
     update(delta){
         this.pos.x+=this.vel.x*delta;
         this.pos.y+=this.vel.y*delta;
+        this.rotation+=this.rotationRate*delta;
     }
 }
-
-
-
-
-
-
-
-
 
 
 
