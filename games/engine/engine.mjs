293167498fa4031c014791246d003e054afb9a8c
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-25 16:18:41 +0200

    Update engine.mjs

--- games/engine/engine.mjs
+++ games/engine/engine.mjs
@@ -1,25 +1,23 @@
 const Vars = {};
-Vars.world = new world(64,64,16);
+Vars.world = new world(64,64);
 
 
 class world{
-    constructor(width, height,tileSize){
+    constructor(width, height){
         this.width=width; //width and height of the world in tiles
         this.height=height;
-        this.tileSize=tileSize; //size of a tile in pixels
         this.objects = [];
     }
-    addObject(object){    
-        this.objects.push(object);
-        return object.id;
-    }
-    removeObject(id){
-        //set dead to true
-        for(let i=0;i<this.objects.length;i++){
-            if(this.objects[i].id==id){
-                this.objects[i].dead=true;
-            }
+    update(delta){
+        for(let i=0; i<this.objects.length; i++){
+            this.objects[i].update(delta);
         }
+        for(let i=0; i<this.objects.length; i++){
+            
+        }
+
+
+
     }
 }
 
@@ -58,41 +56,109 @@ class vector{
         this.x+=Math.cos(angle)*distance;
         this.y+=Math.sin(angle)*distance;
     }
-    toJSON(){
-        return {type:"vector",x:this.x,y:this.y};
-    }
-    static fromJSON(json){
-        return new vector(json.x,json.y);
+    clone(){
+        return new vector(this.x,this.y);
     }
 }
 
 
-class gameObject{
-    constructor(x,y){
-        this.id=Math.floor(Math.random()*1000000);
-        this.groupId=0;
-        this.pos=new pos(x,y);
-        this.rotation=0;
-        this.dead=false;
-        this.collides=false;
-        this.static=true;
-    }
-    update(delta){
-    }
-}
-class movingObject extends gameObject{
-    constructor(x,y,w,h,mass,vel){
-        super(x,y,w,h);
-        this.vel=new vector(vel.x,vel.y);
-        this.rotationRate=0;
+//structure to define a square in 2d
+//should contain mass, x/y movement and angular movement
+class square{
+    constructor(mass,pos,vel,angle)
+    {
         this.mass=mass;
+        this.pos=pos; //center of mass
+        this.vel=vel;
+        this.angle=angle;
+        this.angularVel=0;
     }
     update(delta){
-        this.pos.x+=this.vel.x*delta;
-        this.pos.y+=this.vel.y*delta;
-        this.rotation+=this.rotationRate*delta;
+        this.pos.translate(this.angle,this.vel.x*delta);
+        this.pos.translate(this.angle+Math.PI/2,this.vel.y*delta);
+        this.angle+=this.angularVel*delta;
+    }
+
+    //collision detection
+    //returns true if the square is colliding with the other square	
+    collides(other){
+        //rotate the other square
+        let otherRotated = other.clone();
+        otherRotated.rotate(-this.angle);
+        //rotate the square
+        let rotated = this.clone();
+        rotated.rotate(-other.angle);
+        //move the other square to the origin
+        otherRotated.translate(-this.pos.x,-this.pos.y);
+        //move the square to the origin
+        rotated.translate(-other.pos.x,-other.pos.y);
+        //check if the squares are overlapping
+        if(Math.abs(rotated.pos.x)<=otherRotated.radius && Math.abs(rotated.pos.y)<=otherRotated.radius){
+            //check if the squares are overlapping on the x axis
+            if(Math.abs(rotated.pos.x)<=otherRotated.radius){
+                //check if the squares are overlapping on the y axis
+                if(Math.abs(rotated.pos.y)<=otherRotated.radius){
+                    //collision detected
+                    return true;
+                }
+            }
+        }
+        //no collision detected
+        return false;
+    }
+    collide(other){
+        if(this.collides(other)){
+        //respect inertia and angular velocity
+        let thisRotated = this.clone();
+        thisRotated.rotate(-other.angle);
+        let otherRotated = other.clone();
+        otherRotated.rotate(-this.angle);
+        //move the other square to the origin
+        otherRotated.translate(-this.pos.x,-this.pos.y);
+        //move the square to the origin
+        thisRotated.translate(-other.pos.x,-other.pos.y);
+        //calculate the angle between the squares
+        let angle = Math.atan2(thisRotated.pos.y,thisRotated.pos.x);
+        //calculate the distance between the squares
+        let distance = thisRotated.pos.radius;
+        //calculate the overlap
+        let overlap = distance - otherRotated.radius;
+        //calculate the impulse
+        let impulse = -(1+0.5)*overlap;
+        //calculate the impulse force
+        let impulseForce = new vector(Math.cos(angle)*impulse,Math.sin(angle)*impulse);
+        //apply the impulse force to the squares
+        thisRotated.vel.translate(impulseForce.x,impulseForce.y);
+        otherRotated.vel.translate(-impulseForce.x,-impulseForce.y);
+        //calculate the angular impulse
+        let angularImpulse = -(1+0.5)*(thisRotated.pos.x*otherRotated.pos.y-thisRotated.pos.y*otherRotated.pos.x)/(thisRotated.pos.radius*otherRotated.pos.radius);
+        //calculate the angular impulse force
+        let angularImpulseForce = angularImpulse*thisRotated.massInertia;
+        //apply the angular impulse force to the squares
+        thisRotated.angularVel+=angularImpulseForce;
+        otherRotated.angularVel-=angularImpulseForce;
+        //rotate the squares back
+        thisRotated.rotate(this.angle);
+        otherRotated.rotate(other.angle);
+        //move the squares back to their original position
+        thisRotated.translate(this.pos.x,this.pos.y);
+        otherRotated.translate(other.pos.x,other.pos.y);
+        //update the squares
+        this.update(0);
+        other.update(0);
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
