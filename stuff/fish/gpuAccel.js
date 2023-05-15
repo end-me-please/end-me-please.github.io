ca19a3f2c0f06486a044c47d3e52beb4173b8fdf
end-me-please <90796271+end-me-please@users.noreply.github.com>
2023-05-15 18:31:13 +0200

    bad idea

---
+++ stuff/fish/gpuAccel.js
@@ -0,0 +1,35 @@
+let gpu = new GPU();
+
+
+
+let runLayer = gpu.createKernel(function (input, weights, biases,inputSize, outputSize) {
+    let sum = 0;
+    
+}, {
+    output: [5],
+    dynamicOutput: true,
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
