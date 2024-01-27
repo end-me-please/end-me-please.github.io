65bb139ae2369323d0ce22f9a17f948045d71993
end-me-please <90796271+end-me-please@users.noreply.github.com>
2024-01-27 19:22:21 +0100

    aaaaaaaaaaaaaaaaaaa

--- games/snails/proceduralsnails.js
+++ games/snails/proceduralsnails.js
@@ -88,9 +88,3 @@ class SnailGen {
     }
     
 }
-function randomColor() {
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() *255);
-    return "rgb(" + r + "," + g + "," + b + ")";
-}
