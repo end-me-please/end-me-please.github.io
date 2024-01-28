7ddf6b2082c3462a14f9f5214fd7b3a6b2ef3523
end-me-please <90796271+end-me-please@users.noreply.github.com>
2024-01-28 17:47:33 +0100

    make things better? may b e

--- games/snails/proceduralsnails.js
+++ games/snails/proceduralsnails.js
@@ -6,6 +6,7 @@ class SnailGen {
         this.spiralPitch = spiralPitch;
         this.eyeLength = eyeLength;
         this.radius = 32;
+        this.randomSeed = Math.random();
     }
     draw(ctx){
         
@@ -85,6 +86,19 @@ class SnailGen {
         ctx.fill();
         ctx.closePath();
 
+        
+
+
     }
     
+}
+
+function* seedRandom(seed) {
+    let seed1 = seed;
+    let seed2 = seed;
+    while(true) {
+        seed1 = (seed1 * 9301 + 49297) % 233280;
+        seed2 = (seed2 * 49297 + 233280) % 9301;
+        yield (seed1 + seed2) / 233280;
+    }
 }
